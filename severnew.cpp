8cf9adf70e512013e831c237b016c1da0f17a02a
Picklesword <picklesword@gmail.com>
2015-04-10 12:47:00 -0600

    Updated Server

--- severnew.cpp
+++ severnew.cpp
@@ -11,104 +11,139 @@
 #include <stdlib.h>
 #include <string>
 #include <pthread.h>
+
 using namespace std;
 
-void *task1(void *);
+void *connect(void *);
+
+void open_spreadsheet(char *file_name);
 
 static int connFd;
 
 int main()
 {
-    int pId, portNo, listenFd;
-    int size = 10;
-    socklen_t len; //store size of the address
-    bool loop = false;
-    struct sockaddr_in svrAdd, clntAdd;
-    
-    pthread_t threadA[size];
-    
-    portNo = 2112;
-    
-    
-    //create socket
-    listenFd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if(listenFd < 0)
-    {
-        cerr << "Cannot open socket" << endl;
-        return 0;
-    }
-    
-    bzero((char*) &svrAdd, sizeof(svrAdd));
-    
-    svrAdd.sin_family = AF_INET;
-    svrAdd.sin_addr.s_addr = INADDR_ANY;
-    svrAdd.sin_port = htons(portNo);
-    
-    //bind socket
-    if(bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0)
-    {
-        cerr << "Cannot bind" << endl;
-        return 0;
-    }
-    
-    listen(listenFd, 5);
-    
-    len = sizeof(clntAdd);
-    
-    int noThread = 0;
+	int pId, portNo, listenFd;
+	int size = 10;
+	socklen_t len; //store size of the address
+	bool loop = false;
+	struct sockaddr_in svrAdd, clntAdd;
 
-    while (true)
-    {
-        cout << "Listening" << endl;
+	pthread_t threadA[size];
 
-        //this is where client connects. svr will hang in this mode until client conn
-        connFd = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
+	portNo = 2112;
 
-        if (connFd < 0)
-        {
-            cerr << "Cannot accept connection" << endl;
-            return 0;
-        }
-        else
-        {
-            cout << "Connection successful" << endl;
-        }
-        
-        pthread_create(&threadA[noThread], NULL, task1, NULL); 
-        
-        noThread++;
-    }
-    
-    for(int i = 0; i < noThread; i++)
-    {
-        pthread_join(threadA[i], NULL);
-    }
-    
-    
+
+	//create socket
+	listenFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (listenFd < 0)
+	{
+		cerr << "Cannot open socket" << endl;
+		return 0;
+	}
+
+	bzero((char*)&svrAdd, sizeof(svrAdd));
+
+	svrAdd.sin_family = AF_INET;
+	svrAdd.sin_addr.s_addr = INADDR_ANY;
+	svrAdd.sin_port = htons(portNo);
+
+	//bind socket
+	if (bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0)
+	{
+		cerr << "Cannot bind" << endl;
+		return 0;
+	}
+
+	listen(listenFd, 5);
+
+	int noThread = 0;
+
+	while (true)
+	{
+		cout << "Listening" << endl;
+		socklen_t len = sizeof(clntAdd);
+
+		//this is where client connects. svr will hang in this mode until client conn
+		connFd = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
+
+		if (connFd < 0)
+		{
+			cerr << "Cannot accept connection" << endl;
+			return 0;
+		}
+		else
+		{
+			cout << "Connection successful" << endl;
+		}
+
+		pthread_create(&threadA[noThread], NULL, connect, NULL);
+
+		noThread++;
+	}
+
+	for (int i = 0; i < noThread; i++)
+	{
+		pthread_join(threadA[i], NULL);
+	}
 }
 
-void *task1 (void *dummyPt)
+// The first connection received by a client will be checked here.
+void *connect(void *dummyPt)
 {
-    cout << "Thread No: " << pthread_self() << endl;
-    char test[300];
-    bzero(test, 301);
-    bool loop = false;
-    while(!loop)
-    {    
-        bzero(test, 301);
-        
-        
-        read(connFd, test, 300);
-        
-        string tester (test);
-        cout << tester << endl;
-        cout << tester << endl;
-        
-        
-        if(tester == "exit")
-            break;
-    }
-    cout << "\nClosing thread and conn" << endl;
-    close(connFd);
+
+	cout << "Thread No: " << pthread_self() << endl;
+	char buffer[300];
+	char * parsed;
+	bzero(buffer, 301);
+	bool loop = false;
+	while (!loop)
+	{
+		//Reads from connected client
+		bzero(buffer, 301);
+		read(connFd, buffer, 300);
+
+		//Parse command for checking
+		parsed = strtok(buffer, " \n\r");
+
+
+		cout << parsed << endl;
+		cout << strcmp(parsed, "connect") << endl;
+
+		//Client requests to connect to server with their username
+		if (!strcmp(parsed, "connect"))
+		{
+			send(connFd, "one more thing to understand is how big can this be\n", 45, MSG_NOSIGNAL);
+		}
+
+		//Client requests that a new user be granted access to the server
+		if (!strcmp(parsed, "register user"))
+		{
+		}
+
+		//Client requests to change a cell within the spreadsheet
+		if (!strcmp(parsed, "cell"))
+		{
+		}
+
+
+		if (!strcmp(parsed, "undo"))
+		{
+		}
+
+		while (parsed != NULL)
+		{
+			cout << parsed << endl;
+			parsed = strtok(NULL, " ");
+		}
+
+		string tester(buffer);
+
+
+
+		if (tester == "exit")
+			break;
+	}
+	cout << "\nClosing thread and conn" << endl;
+	close(connFd);
 }
